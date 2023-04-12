fc14af1150ffb341b0e41cd73a50415b3ca9252d
den-Master-2040 <107744290+den-Master-2040@users.noreply.github.com>
2023-04-12 22:17:28 +0300

    Update client.cpp

--- Chess_Windows/client.cpp
+++ Chess_Windows/client.cpp
@@ -9,3 +9,64 @@ Client::Client(QString ipAddres, int port)
 
 }
 
+/*
+ * #include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    socket->connectToHost("192.168.0.96", 2323);
+}
+
+void MainWindow::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_2);
+    out << str;
+    socket->write(Data);
+}
+
+void MainWindow::slotReadyRead()
+{
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_2);
+    if(in.status() == QDataStream::Ok)
+    {
+        QString str;
+        in >>str;
+        ui->textBrowser->append(str);
+    }
+    else
+    {
+        ui->textBrowser->append("Error read");
+    }
+
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    SendToServer(ui->lineEdit->text());
+}
+
+
+*/
